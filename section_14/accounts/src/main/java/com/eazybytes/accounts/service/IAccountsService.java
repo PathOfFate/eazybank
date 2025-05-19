02312105a0c906c0175cfe826f2b645213e98eb4
PathOfFate <h.aleksieiev@gmail.com>
2025-05-19 19:45:38 +0200

    almost complited section 14

---
+++ section_14/accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -0,0 +1,17 @@
+package com.eazybytes.accounts.service;
+
+import com.eazybytes.accounts.dto.CustomerAccountsDto;
+import com.eazybytes.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+
+    void createAccount(CustomerDto customerDto);
+
+    CustomerAccountsDto fetchAccount(String mobileNumber);
+
+    boolean updateAccount(CustomerAccountsDto customerDto);
+
+    boolean deleteAccount(String mobileNumber);
+
+    boolean updateCommunicationStatus(Long accountNumber);
+}
